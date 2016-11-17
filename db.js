1064495d88c208cf18c9dd042d62c961c97f1bd0
Sareen(Ziyun) Zhang <sareenzhang@SareenZnmatoAir.fios-router.home>
2016-11-16 22:34:02 -0500

    2nd m ready to deploy

--- db.js
+++ db.js
@@ -42,4 +42,23 @@ mongoose.model('Tab', Tab);
 mongoose.model('Page', Page);
 
 
-mongoose.connect('mongodb://localhost/project');
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+if (process.env.NODE_ENV == 'PRODUCTION') {
+    // if we're in PRODUCTION mode, then read the configration from a file
+    // use blocking file io to do this...
+    var fs = require('fs');
+    var path = require('path');
+    var fn = path.join(__dirname, 'config.json');
+    var data = fs.readFileSync(fn);
+
+    // our configuration file will be in json, so parse it and set the
+    // connection string appropriately!
+    var conf = JSON.parse(data);
+    var dbconf = conf.dbconf;
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb://localhost/project';
+}
+
+
+mongoose.connect(dbconf);
